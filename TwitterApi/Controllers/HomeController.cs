7cb3d5ae6a8a47e0de85959c7864537eea15bc6e
jyerel <josey_mayor@hotmail.com>
2021-04-01 12:43:35 +0100

    Add project files.

---
+++ TwitterApi/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterApi.Controllers.Twitter;
+using TwitterApi.Models;
+
+namespace TwitterApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult More()
+        {
+            return View();
+        }
+
+        public ActionResult TwitterHome()
+        {
+            var model = TwitterFeeds.GetTwitterFeeds();
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TwitterApi/Data/Twitter/TwitterFeeds.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TwitterApi.Models;
+
+namespace TwitterApi.Controllers.Twitter
+{
+    public class TwitterFeeds
+    {
+        public static HttpClient TwitterApiClient { get; set; } = new HttpClient();
+
+        public static List<TwitterFeedModel> GetTwitterFeeds()
+        {
+            TwitterApiClient = SetTokenBaseData();
+
+            // Post token body content
+            var content = new FormUrlEncodedContent(SetContentValues());
+            var base64EncodedAuthenticationString = EncodeCredentials();
+            var requestMessage = SetPostContent(content, base64EncodedAuthenticationString);
+
+            // Make the request
+            var task = TwitterApiClient.SendAsync(requestMessage);
+            var response = task.Result;
+            response.EnsureSuccessStatusCode();
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            // Fetch Token
+            var accessToken = GrabTokenInString(responseBody);
+
+
+            var twitterFeed = GetTweets(accessToken);
+
+            return twitterFeed;
+        }
+
+
+        public static List<TwitterFeedModel> GetTweets(string token)
+        {
+            var requestUserTimeline = new HttpRequestMessage(HttpMethod.Get, string.Format($"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=BBC&count=6"));
+            requestUserTimeline.Headers.Add("Authorization", "Bearer " + token);
+            var responseUserTimeLine = TwitterApiClient.SendAsync(requestUserTimeline).Result;
+            string responseTwitter = responseUserTimeLine.Content.ReadAsStringAsync().Result;
+
+            // Fetch Json Result
+            var twitterFeeds = JsonConvert.DeserializeObject<List<TwitterFeedModel>>(responseTwitter);
+            return twitterFeeds;
+        }
+
+        private static HttpClient SetTokenBaseData()
+        {
+            if (TwitterApiClient.BaseAddress == null)
+            {
+                Uri baseUri = new Uri("https://api.twitter.com");
+                TwitterApiClient.BaseAddress = baseUri;
+            }
+
+            TwitterApiClient.DefaultRequestHeaders.Clear();
+            TwitterApiClient.DefaultRequestHeaders.ConnectionClose = true;
+
+            return TwitterApiClient;
+        }
+
+        private static string EncodeCredentials()
+        {
+            string authenticationString = "DSnZt2yM4GHGxjZDe1oyrrf0Q:cnHL52gB4dSesiyejTONs01wajvPlNSdsjW6Yj0GDhNwxrtiY1";
+            return Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authenticationString));
+        }
+
+        public static List<KeyValuePair<string, string>> SetContentValues()
+        {
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            return values;
+        }
+
+        public static HttpRequestMessage SetPostContent(FormUrlEncodedContent contents, string AuthenticationString)
+        {
+            var requestMessages = new HttpRequestMessage(HttpMethod.Post, "/oauth2/token");
+            requestMessages.Headers.Authorization = new AuthenticationHeaderValue("Basic", AuthenticationString);
+            requestMessages.Content = contents;
+            return requestMessages;
+        }
+
+        public static string GrabTokenInString(string response)
+        {
+            return response.Substring(response.IndexOf("access_token\":\"") + "access_token\":\"".Length, response.IndexOf("\"}") - (response.IndexOf("access_token\":\"") + "access_token\":\"".Length));
+        }
+    }
+}
